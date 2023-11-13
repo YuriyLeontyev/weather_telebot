5ecc706bc9a0453b01694d791b078bd877ed3d57
YuriyLeontyev <yurale102084@gmail.com>
2023-11-13 23:19:29 +0600

    first commit

---
+++ Weatherbot.py
@@ -0,0 +1,32 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot("6935777768:AAHpHqLmbT91dQFZEhnfgjgnSum-s5TZDro")
+
+API = 'fdf27e8c40ec674281eec42633dcdc31'
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message,'Введи назание города')
+    #bot.register_next_step_handler( message, get_weather)
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        wind = data["wind"]["speed"]
+        pressure = data["main"]["pressure"]
+        bot.reply_to(message, f'Температура: {temp} \n Ветер: {wind}\n Давление: {pressure}')
+
+        image = 'warm.jpg' if temp >5.0 else 'cold.jpg'
+        file = open('./' + image,'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, "Че за город? Пиши корректно!")
+
+
+bot.infinity_polling()
